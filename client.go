cf6a8744918a9eee280ab64d2727bffd41ed421b
PriymakM <priymakmykhailo@gmail.com>
2026-02-13 23:57:40 +0200

    Implemented crud proto for biometric service

---
+++ client.go
@@ -0,0 +1,89 @@
+package client
+
+import (
+	"fmt"
+
+	"github.com/MediStatTech/biometric-client/client_options"
+	services_v1 "github.com/MediStatTech/biometric-client/pb/go/services/v1"
+	log "github.com/MediStatTech/logger"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	xdscreds "google.golang.org/grpc/credentials/xds"
+	_ "google.golang.org/grpc/xds" // Import XDS support for production service discovery
+)
+
+type Facade struct {
+	conn *grpc.ClientConn
+	log  *log.Logger
+
+	// Services clients
+	DiseasSensor  services_v1.DiseasSensorServiceClient
+	Diseas        services_v1.DiseasServiceClient
+	Sensor        services_v1.SensorServiceClient
+	SensorPatient services_v1.SensorPatientServiceClient
+}
+
+func New(
+	o *client_options.Options,
+) (*Facade, error) {
+	var target string
+
+	// Check if custom address is provided
+	if o.AddressName != "" {
+		target = o.AddressName
+	} else {
+		// XDS service discovery for production
+		// This will resolve to the service endpoint via xDS control plane
+		target = "todo-service.svc.cluster.local:8443"
+	}
+
+	// Connection options
+	dialOpts := []grpc.DialOption{
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), // Load balancing
+	}
+
+	// For development, use insecure connection
+	// For production with XDS, use XDS credentials with insecure fallback
+	if o.ENV != nil && o.ENV.IsDev() {
+		dialOpts = append(dialOpts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	} else if o.AddressName != "" && (len(o.AddressName) < 6 || o.AddressName[:6] != "xds://") {
+		// Custom non-XDS address, use insecure
+		dialOpts = append(dialOpts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	} else {
+		// For XDS addresses in non-dev environments, use XDS credentials
+		creds, err := xdscreds.NewClientCredentials(xdscreds.ClientOptions{
+			FallbackCreds: insecure.NewCredentials(),
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to create xds credentials: %w", err)
+		}
+		dialOpts = append(dialOpts, grpc.WithTransportCredentials(creds))
+	}
+
+	conn, err := grpc.NewClient(target, dialOpts...)
+	if err != nil {
+		o.Log.Error("Failed to connect to auth service", map[string]interface{}{
+			"target": target,
+			"error":  err.Error(),
+		})
+		return nil, err
+	}
+
+	o.Log.Info("Connected to auth service", map[string]interface{}{
+		"target": target,
+	})
+
+	return &Facade{
+		conn:          conn,
+		log:           o.Log,
+		DiseasSensor:  services_v1.NewDiseasSensorServiceClient(conn),
+		Diseas:        services_v1.NewDiseasServiceClient(conn),
+		Sensor:        services_v1.NewSensorServiceClient(conn),
+		SensorPatient: services_v1.NewSensorPatientServiceClient(conn),
+	}, nil
+}
+
+// Close closes the gRPC connection
+func (c *Facade) Close() error {
+	return c.conn.Close()
+}
